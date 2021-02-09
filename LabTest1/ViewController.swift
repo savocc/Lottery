98ad1fa5634bb6ef3ae955be6fd41b601c2a1a69
Vladislav Chernyi <6942444+Wladislao@users.noreply.github.com>
2021-02-09 14:06:50 +0300

    Initial Commit

---
+++ LabTest1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LabTest1
+//
+//  Created by Vladislav Chernyi on 09.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
